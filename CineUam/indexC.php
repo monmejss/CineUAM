c5713599931c893e55696f132a43025013f87404
monica <122710250+monmejss@users.noreply.github.com>
2024-05-17 01:05:22 -0600

    Delete CineUam directory

--- CineUam/indexC.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("Controlador/ControladorCartelera.php");
-include_once("Vista/cartelera.php");
-
-$controller = new ControladorCartelera();
-$funciones = $controller->getFunciones();
-
-mostrarCartelera($funciones);
-?>
